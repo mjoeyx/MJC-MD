cb72cf0f881e41ff646efcd8a5bca296b7e88b79
mjc <affantauhid313@gmail.com>
2025-02-02 19:08:50 +0700

    Add files via upload

---
+++ key.js
@@ -0,0 +1,9 @@
+{
+  "secretCode": "mjcx404",
+  "description": "PREMIUM KEY", 
+  "lastUpdated": "02-02-2025",
+  "author": "Mjoeyx", 
+  "status": "Active",
+  "license": "MIT",
+  "version": "3.0.0"
+}
